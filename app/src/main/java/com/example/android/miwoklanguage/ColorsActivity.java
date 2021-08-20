e1ad1399b38bfa1fc1e395b898ee363e8bca75be
Chitresh Mathur <chitreshmathur2001@gmail.com>
2021-08-21 02:16:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/miwoklanguage/ColorsActivity.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwoklanguage;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android.miwok.R;
+
+import java.util.ArrayList;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_list);
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();// generic class provided here is of type word because we make custom class i.e word.java to pass two parameters(one for miwok translation and another one for english translation)
+
+        //words.add("one");--> use when generic class is string
+
+        /*Word w = new Word("one","lutti");
+        words.add(w);//we have made the new object and stored it in variable w then added that variable to our words list
+        to use in very concise way it is equal to.....*/
+        words.add(new Word("red","weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        /*ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item, words); // the arrayadapter can't fullfill the need as it provide list of one one textview so we make a custom adapter class i.e. Word adapter to fulfill the need*/
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words);
+
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/android/miwoklanguage/Word.java
@@ -0,0 +1,55 @@
+package com.example.android.miwoklanguage;
+
+/*****************************************************************************
+this class simply increase the parameters i.e. two for miwok and english word
+ instead of containing one parameter . Work as a generic class for others.
+******************************************************************************/
+
+public class Word {
+
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    /**Drawable reference ID */
+    //private String mImageResourceId;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        //mImageResourceId = imageResourceId;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    /**
+     * Get the image Resource ID
+     */
+    /*public String getmImageResourceId() {
+        return mImageResourceId;
+    }*/
+}
+
+
+
